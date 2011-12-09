f5c5fdac7c5dab84a9a36d288d89330c84a0378e
Joseph Cross <joe.mcross@gmail.com>
2011-12-08 23:53:21 -0500

    Added Util class, cleaned up and extended BaseComponent interface.
    Added OnChange() method to ComUtil.  I've written some varaition of
    this so many times, it's awesome to have finally abstracted it out.
    Weee!

--- src/Components/BaseComponent.py
+++ src/Components/BaseComponent.py
@@ -13,7 +13,9 @@ class BaseComponent(object):
 
 
     def __init__(self, parent=None, x=0, y=0, width=0, height=0,
-                 anchor_x="left", anchor_y="top", coords="local"):
+                 anchor_x="left", anchor_y="top", coords="local",
+                 name="BaseComponent", tooltip="Empty ToolTip",
+                 visible = True, enabled = True):
         '''
         Pass a screen as parent if this is DIRECTLY attached to the screen
         
@@ -23,24 +25,32 @@ class BaseComponent(object):
             Coordinates are stored in local, and transformed to global.
         '''
         
-        #Either a Component or a Screen
-        self._Parent = parent
         
+        #Either a Component or a Screen
+        self._Parent = None
         #Components contained in this Component
         self._Children = []
         
-        self._Name = "BaseComponent"
+        self._Name = name
         self._ID = ComUtil._get_next_id()
         
         self.__IsContentLoaded = False
-        self._Visible = False
-        self._Enabled = True
+        self._Visible = visible
+        self._Enabled = enabled
         self._HasFocus = False
         
         self._TabIndex = 0
-        self._Tooltip = "Empty Tooltip"
+        self._Tooltip = tooltip
         
-        self._position(x, y, width, height, anchor_x, anchor_y)
+        self._x = x
+        self._y = y
+        self._anchor_x = anchor_x
+        self._anchor_y = anchor_y
+        self._width = width
+        self._height = height
+        
+        self.Parent = parent
+            
 
     def get_screen(self):
         #No parent, no screen
@@ -78,7 +88,30 @@ class BaseComponent(object):
         return self._TabIndex
     def get_tooltip(self):
         return self._Tooltip
-
+    
+    def get_x(self):
+        return self._x
+    def get_y(self):
+        return self._y
+    def get_width(self):
+        return self._width
+    def get_height(self):
+        return self._height
+    def get_anchor_x(self):
+        return self._anchor_x
+    def get_anchor_y(self):
+        return self._anchor_y
+    
+    @ComUtil.OnChange("_Parent", "_Needs_Redraw")
+    def set_parent(self, parent):
+        if self.Parent is not None:
+            self.Parent.RemoveChild(self)
+        if parent is not None:
+            parent.AddChild(self)
+        else:
+            self.Visible = False
+        self._Parent = parent
+    
     def set_name(self, value):
         self._Name = value    
     def set_visible(self, value):
@@ -91,12 +124,39 @@ class BaseComponent(object):
             #only undraw if drawn
         if not value and self.IsContentLoaded:
             self._UnloadContent()        
+    
+    @ComUtil.OnChange("_Enabled", "_Needs_Redraw")
     def set_enabled(self, value):
         self._Enabled = value
+        
     def set_tab_index(self, value):
         self._TabIndex = value
     def set_tooltip(self, value):
         self._Tooltip = value
+    
+    @ComUtil.OnChange("_x", "_Needs_Redraw")
+    def set_x(self, value):
+        self._x = value
+    
+    @ComUtil.OnChange("_y", "_Needs_Redraw")
+    def set_y(self, value):
+        self._y = value
+    
+    @ComUtil.OnChange("_width", "_Needs_Redraw")
+    def set_width(self, value):
+        self._width = value
+    
+    @ComUtil.OnChange("_height", "_Needs_Redraw")
+    def set_height(self, value):
+        self._height = value
+    
+    @ComUtil.OnChange("_anchor_x", "_Needs_Redraw")
+    def set_anchor_x(self, value):
+        self._anchor_x = value.lower()
+    
+    @ComUtil.OnChange("_anchor_y", "_Needs_Redraw")
+    def set_anchor_y(self, value):
+        self._anchor_y = value.lower()
     
     
     def _LoadContent(self):
@@ -104,10 +164,13 @@ class BaseComponent(object):
         self.__IsContentLoaded = True
     def _UnloadContent(self):
         """Protected method for unloading graphical content when Visible set to False (from True)"""
-        self.__IsContentLoaded = False        
+        self.__IsContentLoaded = False
+    def _ReloadContent(self):
+        """Protected method for reloading graphical content.  Called when x, y, width, height, anchor_x, anchor_y change."""
+        self._Needs_Redraw = False        
 
     Screen = property(get_screen, None, None, "The screen which this is contained in")
-    Parent = property(get_parent, None, None, "The Component this is contained in")
+    Parent = property(get_parent, set_parent, None, "The Component this is contained in")
     Children = property(get_children, None, None, "Copy of the Component's child Components")
     Name = property(get_name, set_name, None, "Sting used to describe the Component")
     ID = property(get_id, None, None, "Unique ID to reference the Component")
@@ -118,6 +181,37 @@ class BaseComponent(object):
                                "If the Component has loaded necessary drawing content")
     TabIndex = property(get_tab_index, set_tab_index, None, "The index for tabbing through Components")
     Tooltip = property(get_tooltip, set_tooltip, None, "Component tooltip for hover")
-        
+    
+    x = property(get_x, set_x, None, "Global x position")
+    y = property(get_y, set_y, None, "Global y position")
+    width = property(get_width, set_width, None, "Width of the Component")
+    height = property(get_height, set_height, None, "Height of the Component")
+    anchor_x = property(get_anchor_x, set_anchor_x, None, "Where the Component is connected to its x value. [left, center, right]")
+    anchor_y = property(get_anchor_y, set_anchor_y, None, "Where the Component is connected to its y value. [top, center, bottom]")
+    
+    def AddChild(self, child):
+        if not ComUtil.contains(self._Children, child):
+            self._Children.append(child)
+    def RemoveChild(self, child):
+        if ComUtil.contains(self._Children, child):
+            self._Children.remove(child)
+    
+    def MigrateParent(self, parent, preserveAbsoluteCoordinates = True):
+        """Allows migration of parents with coordinate preservation"""
+        if preserveAbsoluteCoordinates:
+            x = self.x
+            y = self.y
+            self.Parent = parent
+            self.x = x
+            self.y = y
+        else:
+            self.Parent = parent
+    
+    def Update(self, dt):
+        if self._Needs_Redraw:
+            self._ReloadContent()
+    
+    
+    
         
         
--- src/Components/ComUtil.py
+++ src/Components/ComUtil.py
@@ -6,4 +6,18 @@ def _get_next_id():
     NEXT_ID += 1
     return NEXT_ID - 1
 
-    
+def contains(_list, item):
+    return _list.count(item) > 0
+
+def OnChange(var_name, flagOnChange):
+    """If the self.var_name != new_value, self.flagOnChange = True"""
+    def fn_wrapper(fn):
+        def wrapped_call(self, new_value):
+            old_value = getattr(self, var_name)
+            fn(self, new_value)
+            if new_value != old_value:
+                setattr(self, flagOnChange, True)
+        return wrapped_call
+    return fn_wrapper
+
+            
