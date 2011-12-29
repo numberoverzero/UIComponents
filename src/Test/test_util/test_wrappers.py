40a33769d039f7a5d9f83b918542754479f13f4a
Joseph Cross <joe.mcross@gmail.com>
2011-12-29 09:07:57 -0500

    Started on wrapper testing.
    Implemented tests for on_change, wrote comment outline of tests
    for inject_args testing.  That one's going to be fun.

--- src/Test/test_util/test_wrappers.py
+++ src/Test/test_util/test_wrappers.py
@@ -2,9 +2,64 @@ import unittest
 import Util.Wrappers as Wrappers
 
 class OnChangeTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        class tst_class(object):
+            flag = False
+            _x = 0
+            def _g_x(self):
+                return self._x
+            @Wrappers.on_change('x', 'flag')
+            def _s_x(self, value):
+                self._x = value
+            x = property(_g_x, _s_x)
+            
+        self.tst_cls = tst_class
+    
+    def test_no_change(self):
+        bob = self.tst_cls()
+        self.assertFalse(bob.flag)
+        bob.x = 0
+        self.assertFalse(bob.flag)
+    
+    def test_change(self):
+        bob = self.tst_cls()
+        self.assertFalse(bob.flag)
+        bob.x = 1
+        self.assertTrue(bob.flag)
+        
+    def test_flag_auto_clear(self):
+        """The flag should never be set to false by the wrapper-
+            it must be explicitly dealt with by the class.
+            This test should show that the flag remains true even when
+            given the same value."""
+        bob = self.tst_cls()
+        self.assertFalse(bob.flag)
+        bob.x = 1
+        self.assertTrue(bob.flag)
+        bob.x = 1
+        self.assertTrue(bob.flag)
 
 class InjectArgsTest(unittest.TestCase):
+    #Test out of order args
+    
+    #Test no default args
+    
+    #Test no args
+    
+    #Test not using kwargs raises error
+    
+    #Test all default args
+    
+    #Test mixing arg/kwarg order
+    
+    #Test ignore on all args
+    
+    #Test ignore on middle kwarg
+    
+    #Test ignore on last kwarg
+    
+    #Test ignore on non-kwarg
     pass
 
 def suite():
