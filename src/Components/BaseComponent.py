ed6c23c1de350ff9a3fd91be9cfb5b9df761ea42
Joseph Cross <joe.mcross@gmail.com>
2012-01-09 08:35:44 -0500

    BaseComponent inherits from HasID, more pylint changes.
    Inheriting from HasID cleans up the constructor a bit, gets rid of
    a get method and a property.  Also makes changes to id management easy.
    
    Added more comments to appease the pylint gods.

--- src/Components/BaseComponent.py
+++ src/Components/BaseComponent.py
@@ -5,52 +5,51 @@ Base component, handles layering management, position, selection, etc.
 import Util
 import Engine
 
-print "BaseComponent Loaded."
-
-class BaseComponent(object):
-    '''
+class BaseComponent(Engine.HasID):
+    """
     Base Component from which all components (combo/check boxes, list selection boxes)
     are derived.
-    '''
+    """
     
     #Initial values
-    _Visible = False
-    _Enabled = False
+    _visible = False
+    _enabled = False
     
-    _Parent = None
-    _Children = []
+    _parent = None
+    _children = []
     
-    _HasFocus = False
-    __IsContentLoaded = False
+    _has_focus = False
+    __is_content_loaded = False
+    _needs_redraw = True
     
     _x = _y = 0
     _width = _height = 0
     _anchor_x = 'left'
     _anchor_y = 'top'
     
-    _Name = "BaseComponent"
-    _Tooltip = "Empty Tooltip"
+    _name = "BaseComponent"
+    _tooltip = "Empty Tooltip"
     
     __triggers_redraw = ["x", "y", "width", "height",
                          "anchor_x", "anchor_y", "parent"]
 
-    @Util.Wrappers.inject_args(['coords', 'id_manager'])
+    @Util.Wrappers.inject_args(['coords', 'id_manager','custom_id'])
     def __init__(self, Parent=None, x=0, y=0, width=0, height=0, # pylint: disable-msg=C0103,W0613,C0301
                  anchor_x="left", anchor_y="top", coords="local", # pylint: disable-msg=C0103,W0613,C0301
                  Name="BaseComponent", Tooltip="Empty Tooltip", # pylint: disable-msg=C0103,W0613,C0301
-                 Visible=True, Enabled=True, id_manager=None): # pylint: disable-msg=C0103,W0613,C0301
-        '''
+                 Visible=True, Enabled=True, id_manager=None, # pylint: disable-msg=C0103,W0613,C0301
+                 custom_id=None): # pylint: disable-msg=C0103,W0613,C0301
+        """
         Pass a screen as parent if this is DIRECTLY attached to the screen
         
         coords is either "global" or "local" referring to the value of x and y.
         EX. BaseComponent(None, 20, 20, "local", 10, 10) attached to a component
             with global x,y [50, 100] would have global coords [70, 120].
             Coordinates are stored in local, and transformed to global.
-        '''
+        """
         
-        if id_manager is None:
-            id_manager = Engine.GLOBAL_ID_MANAGER
-        self._id = id_manager.next_id(self)        
+        Engine.HasID.__init__(self, id_manager, custom_id)
+        
         self._tab_index = 0
         if Visible:
             #Make sure that content loads properly, since args may have been
@@ -61,90 +60,122 @@ class BaseComponent(object):
         if name in BaseComponent.__triggers_redraw:
             old_value = getattr(self, name)
             if old_value != value:
-                self._Needs_Redraw = True
+                self._needs_redraw = True
         super(BaseComponent, self).__setattr__(name, value)
     
     def __get_settings(self):
+        """Settings from screen (or parent's screen)"""
         if self.Screen is None:
             return None
         else:
-            return self.Screen.Settings;
+            return self.Screen.Settings
     def __get_screen(self):
+        """The screen this component is attached to
+            (recurses up through parents)"""
         #No parent, no screen
         if self.Parent is None:
             return None
         #If parent is a component, use its screen
         if isinstance(self.Parent, BaseComponent):
             return self.Parent.Screen
-        #Otherwise self._Parent IS the screen
+        #Otherwise self._parent IS the screen
         return self.Parent
     def __get_parent(self):
         """Returns the parent component.
             This is expecting an actual BaseComponent- 
             don't return true unless we 
             ACTUALLY have a Component for a parent."""
-        if isinstance(self._Parent, BaseComponent):
-            return self._Parent
+        if isinstance(self._parent, BaseComponent):
+            return self._parent
         return None
     
     def __get_children(self):
-        return self._Children[:]
+        """Returns a COPY of list of children components"""
+        return self._children[:]
     def __get_name(self):
-        return self._Name
-    def __get_id(self):
-        return self._id
+        """Get the component's name"""
+        return self._name
     def __get_visible(self):
-        return self._Visible
+        """Get the component's visibility state"""
+        return self._visible
     def __get_enabled(self):
-        return self._Enabled
+        """Get the component's enabled state"""
+        return self._enabled
     def __get_has_focus(self):
-        return self._HasFocus
+        """Get the component's focus state"""
+        return self._has_focus
     def __get_is_content_loaded(self):
-        return self.__IsContentLoaded
+        """Returns if the component's content has been loaded"""
+        return self.__is_content_loaded
     def __get_tab_index(self):
+        """Returns the index used for tab control,
+            such as tabbing through fields in a form
+            or shifting focus between two panes"""
         return self._tab_index
     def __get_tooltip(self):
-        return self._Tooltip
+        """Tooltip for the component"""
+        return self._tooltip
     
     def __get_x(self):
+        """Get x coordinate.  Not sure if local or global..."""
         return self._x
     def __get_y(self):
+        """Get y coordinate.  Not sure if local or global..."""
         return self._y
     def __get_width(self):
+        """Get the width of the component"""
         return self._width
     def __get_height(self):
+        """Get the height of the component"""
         return self._height
     def __get_anchor_x(self):
+        """Get the horizontal anchor of the component
+            (where it attaches to parent or screen)"""
         return self._anchor_x
     def __get_anchor_y(self):
+        """Get the vertical anchor of the component
+            (where it attaches to parent or screen)"""
         return self._anchor_y
     
     def __set_parent(self, parent):
+        """Set the component's parent.  Setting to None
+            when the component has a parent will trigger a 
+            remove_child on the current parent, then set to none.
+            If the parent being added is not None, we tell the parent
+            to add the child (this)
+            If parent is set to none, the component is set to
+            invisible."""
         if self.Parent is not None:
             self.Parent.remove_child(self)
         if parent is not None:
             parent.add_child(self)
         else:
-            self.Visible = False
-        self._Parent = parent
+            self.Visible = False #pylint:disable-msg=C0103
+        self._parent = parent
     def __set_name(self, value):
-        self._Name = value    
+        """Set the component's name"""
+        self._name = value    
     def __set_visible(self, value):
-        self._Visible = value
+        """Set the component's visibility state.
+            Lazy redraw if flipping on,
+            immediate unload if flipping off."""
+        self._visible = value
         #Enabling Visible
             #only draw if not drawn
         if value and not self.IsContentLoaded:
-            self._Needs_Redraw = True
+            self._needs_redraw = True
         #Disabling Visible
             #only undraw if drawn
         if not value and self.IsContentLoaded:
             self._UnloadContent()
     def __set_enabled(self, value):
-        self._Enabled = value
+        """Set the component's enabled state"""
+        self._enabled = value
     def __set_tab_index(self, value):
+        """Set the component's tab index"""
         self._tab_index = value
     def __set_tooltip(self, value):
-        self._Tooltip = value
+        self._tooltip = value
     
     def __set_x(self, value):
         self._x = value
@@ -161,23 +192,22 @@ class BaseComponent(object):
     
     def _LoadContent(self):
         """Protected method for loading graphical content when Visible set to True (from False)"""
-        self.__IsContentLoaded = True
-        self._Needs_Redraw = False
+        self.__is_content_loaded = True
+        self._needs_redraw = False
     def _UnloadContent(self):
         """Protected method for unloading graphical content when Visible set to False (from True)"""
-        self.__IsContentLoaded = False
-        self._Needs_Redraw = False
+        self.__is_content_loaded = False
+        self._needs_redraw = False
     def _ReloadContent(self):
         """Protected method for reloading graphical content.  Called when x, y, width, height, anchor_x, anchor_y change."""
-        self.__IsContentLoaded = True
-        self._Needs_Redraw = False        
+        self.__is_content_loaded = True
+        self._needs_redraw = False        
 
     Settings = property(__get_settings)
     Screen = property(__get_screen)
     Parent = property(__get_parent, __set_parent)
     Children = property(__get_children)
     Name = property(__get_name, __set_name)
-    ID = property(__get_id)
     Visible = property(__get_visible, __set_visible)
     Enabled = property(__get_enabled, __set_enabled)
     HasFocus = property(__get_has_focus)
@@ -195,8 +225,8 @@ class BaseComponent(object):
     def add_child(self, child):
         """Adds the Component to Children, as long as the Component
             isn't already considered a child."""
-        if not Util.contains(self._Children, child):
-            self._Children.append(child)
+        if not Util.contains(self._children, child):
+            self._children.append(child)
             
     def contains(self, x, y): # pylint: disable-msg=C0103
         """Returns the Component that contains (x, y)- this allows parent
@@ -204,7 +234,7 @@ class BaseComponent(object):
             child Component if they have one).
             Trys to return the most specific (child-first) container.
             Returns None if not containing the point x,y"""
-        for child in self._Children:
+        for child in self._children:
             #Containing component
             ccomponent = child.contains(x, y)
             if ccomponent is not None:
@@ -246,11 +276,11 @@ class BaseComponent(object):
             #it calls parent.remove.
             #Popping safely removes it from the list0
             #otherwise we'd be doing list.remove during a list iteration
-        while len(self._Children) > 0:
-            child = self._Children.pop()
+        while len(self._children) > 0:
+            child = self._children.pop()
             child.exit_component()
             
-        self._Needs_Redraw = False
+        self._needs_redraw = False
         self.Enabled = False
         self.Visible = False
         self.Parent = None
@@ -268,8 +298,8 @@ class BaseComponent(object):
     def remove_child(self, child):
         """Removes the Component from Children, as long as the Component
             is actually considered a child."""
-        if Util.contains(self._Children, child):
-            self._Children.remove(child)
+        if Util.contains(self._children, child):
+            self._children.remove(child)
     
     def update(self, dt):
         """Derived classes should call this method AFTER their implementation,
@@ -277,5 +307,5 @@ class BaseComponent(object):
             in the frame of their change.  Calling base before overriding
             method logic will effectively apply that logic to the NEXT update
             call, not THIS call."""
-        if self._Needs_Redraw:
+        if self._needs_redraw:
             self._ReloadContent()
