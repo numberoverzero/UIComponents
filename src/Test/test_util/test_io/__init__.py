ff0a8f480469e3fb3b2b37f64e07fce8b137040c
Joseph Cross <joe.mcross@gmail.com>
2012-01-12 18:30:34 -0500

    Re-organized date/time fmt in IO, added io.__init__ testing
    ^ this

--- src/Test/test_util/test_io/__init__.py
+++ src/Test/test_util/test_io/__init__.py
@@ -3,16 +3,22 @@ import Util.IO as IO
 
 class FmtTimeTest(unittest.TestCase):
     def test_fmt_date(self):
-        #IO.file_fmt_date(date_)
-        self.skipTest("Not Yet Implemented")
+        now = IO.time.localtime()
+        actual = IO.file_fmt_date(now)
+        expected = IO.time.strftime(IO.FMT_DATE, now)
+        self.assertEqual(expected, actual)
     
     def test_fmt_time(self):
-        #IO.file_fmt_time(time_)
-        self.skipTest("Not Yet Implemented")
+        now = IO.time.localtime()
+        actual = IO.file_fmt_time(now)
+        expected = IO.time.strftime(IO.FMT_TIME, now)
+        self.assertEqual(expected, actual)
     
     def test_fmt_datetime(self):
-        #IO.file_fmt_datetime(datetime_)
-        self.skipTest("Not Yet Implemented")
+        now = IO.time.localtime()
+        actual = IO.file_fmt_datetime(now)
+        expected = IO.time.strftime(IO.FMT_DATETIME, now)
+        self.assertEqual(expected, actual)
 
 def suite():
     suite1 = unittest.makeSuite(FmtTimeTest)
--- src/Util/IO/__init__.py
+++ src/Util/IO/__init__.py
@@ -3,29 +3,34 @@ Input/Output utility classes and functions
 """
 import time
 
+FMT_DATE = "%Y_%d_%m"
+FMT_TIME = "%H_%M_%S"
+FMT_DATETIME = "%Y_%d_%m__%H_%M_%S"
+
+def _file_fmt(fmt, value):
+    """Applies current time if no time provided,
+        uses fmt as formatting string"""
+    if not value: 
+        value = time.localtime()
+    return time.strftime(fmt, value)
+
 def file_fmt_date(date_ = None):
     """Returns the date in MM_DD_YYYY format.
         Uses localtime if called without args.
         Make sure the passed arg has a strformat method."""
-    fmt = "%Y_%d_%m"
-    if not date_: 
-        date_ = time.localtime()
-    return time.strftime(fmt, date_)
+    fmt = FMT_DATE
+    return _file_fmt(fmt, date_)
 
 def file_fmt_time(time_ = None):
     """Returns the time in HH_MM_SS format.
         Uses localtime if called without args.
         Make sure the passed arg has a strformat method."""
-    fmt = "%H_%M_%S"
-    if not time_: 
-        time_ = time.localtime()
-    return time.strftime(fmt, time_)
+    fmt = FMT_TIME
+    return _file_fmt(fmt, time_)
 
 def file_fmt_datetime(datetime_ = None):
     """Returns the date and time in MM_DD_YYYY__HH_MM_SS format.
         Uses localtime if called without args.
         Make sure the passed arg has a strformat method."""
-    fmt = "%Y_%d_%m__%H_%M_%S"
-    if not datetime_: 
-        datetime_ = time.localtime()
-    return time.strftime(fmt, datetime_)
+    fmt = FMT_DATETIME
+    return _file_fmt(fmt, datetime_)
