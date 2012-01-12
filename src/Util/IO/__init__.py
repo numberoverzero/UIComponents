53bf6cd8db3b83e651e75f13f93e33fcb4f7488b
Joseph Cross <joe.mcross@gmail.com>
2012-01-12 08:47:58 -0500

    Filled out the file_fmt_xx methods in IO.__init__.py
    Not sure these are 100% correct- need to check the time formatting doc,
    and need to make sure it's timetuple.strfttime(fmt) that actually
    formats strings.  Default None -> localtime() works at least =)

--- src/Util/IO/__init__.py
+++ src/Util/IO/__init__.py
@@ -3,14 +3,29 @@ Input/Output utility classes and functions
 """
 import time
 
-def file_fmt_date():
-    """Returns the date in MM_DD_YYYY format"""
-    pass
+def file_fmt_date(date_ = None):
+    """Returns the date in MM_DD_YYYY format.
+        Uses localtime if called without args.
+        Make sure the passed arg has a strformat method."""
+    fmt = "%MM_%DD_%YYYY"
+    if not date_: 
+        date_ = time.localtime()
+    return date_.strftime(fmt)
 
-def file_fmt_time():
-    """Returns the time in HH_MM_SS format"""
-    pass
+def file_fmt_time(time_ = None):
+    """Returns the time in HH_MM_SS format.
+        Uses localtime if called without args.
+        Make sure the passed arg has a strformat method."""
+    fmt = "%HH_%mm_%ss"
+    if not time_: 
+        time_ = time.localtime()
+    return time_.strftime(fmt)
 
-def file_fmt_datetime():
-    """Returns the date and time in MM_DD_YYYY__HH_MM_SS format"""
-    pass
+def file_fmt_datetime(datetime_ = None):
+    """Returns the date and time in MM_DD_YYYY__HH_MM_SS format.
+        Uses localtime if called without args.
+        Make sure the passed arg has a strformat method."""
+    fmt = "%MM_%DD_%YYYY__%HH_%mm_%SS"
+    if not datetime_: 
+        datetime_ = time.localtime()
+    return datetime_.strftime(fmt)
