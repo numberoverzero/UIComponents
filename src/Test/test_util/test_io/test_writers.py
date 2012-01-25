e751217507351df9211145b8085ca86708524f9d
Joseph Cross <joe.mcross@gmail.com>
2012-01-25 09:03:50 -0500

    Added testing skeleton for BufferedWriter and Logger.
    Methods for testing and notes on what to test for each have been added.
    Now I need to implement them.

--- src/Test/test_util/test_io/test_writers.py
+++ src/Test/test_util/test_io/test_writers.py
@@ -15,18 +15,61 @@ class BufferedWriteTest(unittest.TestCase):
         unittest.TestCase.tearDown(self) 
         Util.IO.remove_file(self.filename)
     
-    def test_buffer_sizes(self):
+    def test_write_and_writeln(self):
         #Test 0 buffer
         
         #Test 10 buffer
         
         pass
     
-    def test
+    def test_close_and_force_write(self):
+        #Test 0 buffer
+        
+        #Test 10 buffer
+        
+        pass
+
+class LoggerTest(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        
+        #Where we'll write to
+        self.filename = 'temp_logger_test_file.txt'
+    
+    def tearDown(self):
+        unittest.TestCase.tearDown(self) 
+        Util.IO.remove_file(self.filename)
+    
+    def test_custom_tid(self):
+        #Test that custom starts/stops process in the order
+            #They are passed, not numeric order
+        pass
+    
+    def test_auto_tid(self):
+        #Test that chronological ordering is kept for
+            #tid start/stops.
+        pass
+    
+    def test_log_msg(self):
+        #Test log msg without tid
+        
+        #Test log msg with tid
+        
+        pass
+    
+    def test_log_elapsed(self):
+        #Test log elapsed with tid
+        
+        #Test log elapsed without tid
+        
+        pass
 
 def suite():
+    test_suite = unittest.TestSuite()
     suite1 = unittest.makeSuite(BufferedWriteTest)
-    return unittest.TestSuite(suite1)
+    suite2 = unittest.makeSuite(LoggerTest)
+    test_suite.addTests([suite1, suite2])
+    return test_suite
     
 def load_tests():
     return suite()
