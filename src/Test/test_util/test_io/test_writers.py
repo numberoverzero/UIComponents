68cfc1450d654c9856c43231e8fbb8eae167c63e
Joseph Cross <joe.mcross@gmail.com>
2012-01-26 20:43:29 -0500

    Added tid tests.
    Just need to add the log_msg tests and I'm set for logger testing.
    After some time inspecting different digit counting techniques, I've
    decided that:
    	1) my current method is bad. 200% slower on average.
    	2) I don't understand python strings as well as I think I do.
    I'll be putting up an entry summarizing the differences in a moment.
    (as of this commit date, joecrossdevelopment.wordpress.com)

--- src/Test/test_util/test_io/test_writers.py
+++ src/Test/test_util/test_io/test_writers.py
@@ -99,12 +99,65 @@ class LoggerTest(unittest.TestCase):
     def test_custom_tid(self):
         #Test that custom starts/stops process in the order
             #They are passed, not numeric order
-        pass
+        log = Writers.Logger(self.filename)
+        #Test order: 0( 1( 2( )1 3( )0 )3 )2
+        tids = ['jello', 'pudding', 1, -10]
+        log.start(tids[0])
+        time.sleep(0.35)
+        log.start(tids[1])
+        time.sleep(0.35)
+        log.start(tids[2])
+        time.sleep(0.35)
+        log.stop(tids[1])
+        time.sleep(0.35)
+        log.start(tids[3])
+        time.sleep(0.35)
+        log.stop(tids[0])
+        time.sleep(0.35)
+        log.stop(tids[3])
+        time.sleep(0.35)
+        log.stop(tids[2])
+        
+        #Assert start times are in order
+        start = lambda i: log._time_start[tids[i]]
+        self.assertTrue(start(0) < start(1))
+        self.assertTrue(start(1) < start(2))
+        self.assertTrue(start(2) < start(3))
+        
+        #Assert end times are in order
+        end = lambda i: log._time_end[tids[i]]
+        self.assertTrue(end(1) < end(0))
+        self.assertTrue(end(0) < end(3))
+        self.assertTrue(end(3) < end(2))
+        
     
     def test_auto_tid(self):
+        log = Writers.Logger(self.filename)
         #Test that chronological ordering is kept for
             #tid start/stops.
-        pass
+        tid_start = []
+        tid_end = []
+        for i in xrange(4):
+            tid_start.append(log.start())
+            time.sleep(0.35)
+        for i in xrange(4):
+            tid_end.append(log.stop())
+            time.sleep(0.35)
+        
+        #Assert start times are in order
+        start = lambda i: log._time_start[tid_start[i]]
+        self.assertTrue(start(0) < start(1))
+        self.assertTrue(start(1) < start(2))
+        self.assertTrue(start(2) < start(3))
+        
+        #Assert end times are in order
+        end = lambda i: log._time_end[tid_end[i]]
+        self.assertTrue(end(0) < end(1))
+        self.assertTrue(end(1) < end(2))
+        self.assertTrue(end(2) < end(3))
+        
+        #Assert lists are reversed
+        self.assertListEqual(tid_start, list(reversed(tid_end)))
     
     def test_log_msg(self):
         #Test log msg without tid
